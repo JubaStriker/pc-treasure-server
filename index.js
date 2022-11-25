7fcdd5f72121c8a6b07c9810c349855d5cdfca01
JubaStriker <jubastriker@gmail.com>
2022-11-25 19:50:33 +0600

    server running

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
-const jwt = require('jsonwebtoken');
 require("dotenv").config()
 const app = express();
 
@@ -10,10 +9,13 @@ app.use(cors())
 app.use(express.json());
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.ycsbxjs.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
+
+app.get('/', async (req, res) => {
+    res.send("pc treasure server running")
+});
+
+app.listen(port, () => {
+    console.log(`pc treasure server running on port ${port}`);
 });
